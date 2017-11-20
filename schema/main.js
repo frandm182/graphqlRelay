02cd6a22df3e62995bd54dc10adc0f52d2afa56f
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-11-20 12:56:31 +0100

    chapter1-using-the-schema

--- schema/main.js
+++ schema/main.js
@@ -1,16 +1,16 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 
 const queryType = new GraphQLObjectType({
-    name:'RootQuery',
-    fields: {
-        hello: {
-            type: GraphQLString,
-            resolve: () => 'world'
-        }
+  name: 'RootQuery',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world'
     }
+  }
 });
 const mySchema = new GraphQLSchema({
-    query: queryType
+  query: queryType
 });
 
 module.exports = mySchema;
