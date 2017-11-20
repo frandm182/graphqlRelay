4cd289f488153fb94f40cd2f29e092c649e88eda
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-11-20 14:19:19 +0100

    setting-up-mongodb

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const { graphql } = require('graphql');
 const readline = require('readline');
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
 const mySchema = require('./schema/main');
 
-const rli = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const MONGO_URL = 'mongodb://localhost:27017/test';
 
-rli.question('Client Request: ', inputQuery => {
-  graphql(mySchema, inputQuery).then(result => {
-    console.log('Server Answer: ', result.data);
+MongoClient.connect(MONGO_URL, (err, db) => {
+  assert.equal(null, err);
+  console.log('Connected to MongoDB server');
+  const rli = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
-  rli.close();
+
+  rli.question('Client Request: ', inputQuery => {
+    graphql(mySchema, inputQuery, {}, { db }).then(result => {
+      console.log('Server Answer: ', result.data);
+    });
+    rli.close();
+  });
 });
--- schema/main.js
+++ schema/main.js
@@ -31,6 +31,10 @@ const queryType = new GraphQLObjectType({
 
         return rolls;
       }
+    },
+    usersCount: {
+      type: GraphQLInt,
+      resolve: (_, args, { db }) => db.collection('users').count()
     }
   }
 });
